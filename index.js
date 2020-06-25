3c6504cc84a65f1b8691ee17aabbd3000546a67c
rahimtay <rahimaliev2@mail.ru>
2020-06-25 13:49:49 +0600

    creating fizzbuzz

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var a = "Fizz"
+ var b = "Buzz"
+ for (var i=0;i<100;i++){
+     if(i%3 ==0  && i%5 == 0){
+         console.log (a+b)
+     }else if(i%5 == 0){
+         console.log (b)
+     }
+ }
+
+ 
